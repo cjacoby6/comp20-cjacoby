3ebe50c3a5753192caa496f58287f7686da4bfa3
cjacoby6 <cbeaudetjacoby@gmail.com>
2015-02-27 12:46:27 -0500

    Google board drawing

--- mspacman/game.js
+++ mspacman/game.js
@@ -1,1 +1,10 @@
-// Replace this with your code...
+function init()
+{
+	var ctx = document.getElementById('game_canvas').getContext('2d');
+	var img = new Image();
+	img.src = "pacman10-hp-sprite.png";
+
+	img.onload = function() {
+		ctx.drawImage(img, 319, 0, 465, 137, 0, 0, 464, 135);
+	};
+}
