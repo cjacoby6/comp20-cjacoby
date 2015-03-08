ff2b1fbb9930c8e5820b9a22a2f7d5a6b91d0a47
cjacoby6 <cbeaudetjacoby@gmail.com>
2015-03-08 18:54:07 -0400

    consistency

--- messages/lab.js
+++ messages/lab.js
@@ -7,14 +7,14 @@ messages.onreadystatechange = recieveData;
 messages.send();
 
 function recieveData() {
-	console.log("In my function " + xhr.readyState);
-	if (xhr.readyState == 4 && xhr.status == 200) {
+	console.log("In my function " + messages.readyState);
+	if (messages.readyState == 4 && messages.status == 200) {
 		alert("Got data back!");
 		data = JSON.parse(messages.responseText);
 		var result = "";
 		for (i = 0; i < data.length; i++) {
 			result += "<p>" + data[i]['username'] + " - " + data[i]["content"] + "</p>";
 		}
-		document.getElementById("messages").innerHTML = xhr.responseText;
+		document.getElementById("messages").innerHTML = messages.responseText;
 	}
 }
