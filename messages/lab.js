d719fb994a26e69ca26a423221ea51389f85857d
cjacoby6 <cbeaudetjacoby@gmail.com>
2015-03-08 18:59:56 -0400

    update

--- messages/lab.js
+++ messages/lab.js
@@ -6,17 +6,18 @@ function parse() {
 	messages.onreadystatechange = recieveData;
 
 	messages.send();
+}
 
-	function recieveData() {
-		console.log("In my function " + messages.readyState);
-		if (messages.readyState == 4 && messages.status == 200) {
-			alert("Got data back!");
-			data = JSON.parse(messages.responseText);
-			var result = "";
-			for (i = 0; i < data.length; i++) {
-				result += "<p>" + data[i]['username'] + " - " + data[i]["content"] + "</p>";
-			}
-			document.getElementById("messages").innerHTML = messages.responseText;
+
+function recieveData() {
+	console.log("In my function " + messages.readyState);
+	if (messages.readyState == 4 && messages.status == 200) {
+		alert("Got data back!");
+		data = JSON.parse(messages.responseText);
+		var result = "";
+		for (i = 0; i < data.length; i++) {
+			result += "<p>" + data[i]['username'] + " - " + data[i]["content"] + "</p>";
 		}
+		document.getElementById("messages").innerHTML = messages.responseText;
 	}
 }
