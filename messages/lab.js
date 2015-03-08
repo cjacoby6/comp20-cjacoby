6edef2cde369eb34273b7bb7a027053895324b0b
cjacoby6 <cbeaudetjacoby@gmail.com>
2015-03-08 18:50:31 -0400

    attempt at part 1

--- messages/lab.js
+++ messages/lab.js
@@ -1,1 +1,20 @@
-// Your JavaScript goes here...
+messages = new XMLHttpRequest();
+
+messages.open("get", "data.json", true);
+
+messages.onreadystatechange = recieveData;
+
+messages.send();
+
+function recieveData() {
+	console.log("In my function " + xhr.readyState);
+	if (xhr.readyState == 4 && xhr.status == 200) {
+		alert("Got data back!");
+		data = JSON.parse(messages.responseText);
+		var result = "";
+		for (i = 0; i < data.length; i++) {
+			result += "<p>" + data[i]['username'] + " - " + data[i]["content"] + "</p>";
+		}
+		document.getElementById("messages").innerHTML = xhr.responseText;
+	}
+}
