66a7027226045691d6e7211758aa17fc6d7af35d
cjacoby6 <cbeaudetjacoby@gmail.com>
2015-03-09 12:29:38 -0400

    changed parsing

--- messages/lab.js
+++ messages/lab.js
@@ -12,11 +12,10 @@ function parse() {
 function recieveData() {
 	console.log("In my function " + xml.readyState);
 	if (xml.readyState == 4 && xml.status == 200) {
-		alert("Got data back!");
 		data = JSON.parse(xml.responseText);
 		var result = "";
 		for (i = 0; i < data.length; i++) {
-			result += "<p>" + data[i]['username'] + " - " + data[i]["content"] + "</p>";
+			result += "<p>" + data[i]["content"] + "</p>";
 		}
 		document.getElementById("messages").innerHTML = xml.responseText;
 	}
